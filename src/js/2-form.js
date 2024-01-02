cec4ad5bb835f883441c11f695725d2ff3e5ddcf
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-01-02 15:59:54 +0200

    виконав 2.0

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,56 +1,51 @@
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('input');
-const textarea = document.querySelector('textarea');
 
 form.addEventListener('submit', handleFormSubmit);
-textarea.addEventListener('input', handleTextarea);
-input.addEventListener('input', handleInput);
+form.addEventListener('input', handleInput);
 populateFields();
 
 function handleFormSubmit(e) {
   e.preventDefault();
+
   const formData = getFormData();
   if (
     !formData.email ||
     !formData.message ||
-    formData.email.trim() === '' ||
-    formData.message.trim() === ''
+    !formData.email.trim() ||
+    !formData.message.trim()
   ) {
     return;
   }
+
   console.log(formData);
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
 }
-function handleInput() {
-  const formData = {
-    ...getFormData(),
-    email: input.value.trim(),
-  };
-  saveFormData(formData);
+
+function handleInput(e) {
+  const formData = getFormData();
+  if (e.target.matches('input, textarea')) {
+    const { name, value } = e.target;
+    const trimmedValue = value.trim();
+    formData[name] = trimmedValue;
+    saveFormData(formData);
+  }
 }
 
-function handleTextarea() {
-  const formData = {
-    ...getFormData(),
-    message: textarea.value.trim(),
-  };
-  saveFormData(formData);
-}
 function getFormData() {
   return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 }
+
 function saveFormData(formData) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateFields() {
   const formData = getFormData();
-  if (formData.email) {
-    input.value = formData.email;
-  }
-  if (formData.message) {
-    textarea.value = formData.message;
+  for (const element of form.elements) {
+    if (element.name && formData[element.name]) {
+      element.value = formData[element.name];
+    }
   }
 }
